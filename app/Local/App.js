a79c049f8cfaf0dafd3da3cb6d59ea418318d962
ChiperSoft <chiper@chipersoft.com>
2014-01-27 11:16:22 -0800

    Changed case in App to be more consistant to the rest of the codebase. Disabled express sessions as default.

--- app/Local/App.js
+++ app/Local/App.js
@@ -4,37 +4,38 @@ var flash = require('connect-flash');
 
 var Config = require('Local/Config');
 
-var app = express();
+var App = express();
 
-app.set('view', require('Local/View'));
-app.set('views', __dirname + '/../views/');
+App.set('view', require('Local/View'));
+App.set('views', __dirname + '/../views/');
+
 //handle favicons very first thing, as they don't need any other processing
 App.use(express.favicon(Config.paths.assets + '/favicon.ico'));
 
 // Cookie processing
-app.use(express.cookieParser());
+App.use(express.cookieParser());
 
 // Post/Put Data handling
-app.use(express.json({
+App.use(express.json({
 	limit: Config.maxFormSize
 }));
 
-app.use(express.urlencoded({
+App.use(express.urlencoded({
 	limit: Config.maxFormSize
 }));
 
 // Sessions
-app.use(express.session({
-	store: new connectRedis({
-		host: Config.redis.host,
-		port: Config.redis.port,
-		db:   Config.redis.database.sessions,
-		pass: Config.redis.password
-	}),
+App.use(express.session({
+	// store: new connectRedis({
+	// 	host: Config.redis.host,
+	// 	port: Config.redis.port,
+	// 	db:   Config.redis.database.sessions,
+	// 	pass: Config.redis.password
+	// }),
 	secret: Config.sessions.secret,
 	key: Config.sessions.cookieKey
 }));
 
-app.use(flash());
+App.use(flash());
 
-module.exports = app;
+module.exports = App;
